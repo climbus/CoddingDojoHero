753314183d5043d8636c380708e00693aec9fc07
Climbus <climbus@gmail.com>
2015-04-04 22:10:26 +0200

    refaktoring krok 2

--- tests/codding_box_test.js
+++ tests/codding_box_test.js
@@ -3,6 +3,7 @@ describe("codding box", function() {
     var exampleFileName = "example.txt";
     var exampleContent = "Example";
     var exampleId = "exampleId";
+    var element;
 
     beforeEach(function() {
         spyOn(jQuery, "get").and.callFake(function(filename, callback) {
@@ -12,17 +13,19 @@ describe("codding box", function() {
         spyOn(jQuery, "post").and.callFake(function(filename, data, callback) {
             expect(data.data).toBe(exampleContent);
         });
+        
+        element = document.createElement("div");
     });
 
     it("should load editor", function() {
-        var element = document.createElement("div");
+        
         var box = new CoddingBox(element);
         expect(box.editor).toBeDefined();
     });
 
     it("should load editor on id", function() {
         var filename = exampleFileName;
-        var element = document.createElement("div");
+
         element.id = exampleId;
 
         document.body.appendChild(element);
@@ -34,7 +37,6 @@ describe("codding box", function() {
     it("should load file", function() {
         
         var filename = exampleFileName;
-        var element = document.createElement("div");
         var box = new CoddingBox(element, filename);
         expect(box.editor.getValue()).toMatch(exampleContent);
     });
@@ -42,7 +44,6 @@ describe("codding box", function() {
     it("should save file", function() {
 
         var filename = exampleFileName;
-        var element = document.createElement("div");
         var box = new CoddingBox(element, filename);
         box.editor.setValue(exampleContent);
         box.save();
@@ -52,21 +53,18 @@ describe("codding box", function() {
 
     it("should show file name", function() {
         var filename = exampleFileName;
-        var element = document.createElement("div");
         var box = new CoddingBox(element, filename);
         expect(element.innerHTML).toMatch(filename);
     });
 
     it("should has button", function() {
         var filename = exampleFileName;
-        var element = document.createElement("div");
         var box = new CoddingBox(element, filename);
         expect(element.innerHTML).toMatch("<button>");
     });
 
     it("button should be clicking and saving content", function() {
         var filename = exampleFileName;
-        var element = document.createElement("div");
         var box = new CoddingBox(element, filename);
         $(element.getElementsByTagName("button")[0]).click();
         expect(jQuery.post.calls.any()).toBe(true);
